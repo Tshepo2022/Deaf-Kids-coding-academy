13829803ff2eef5c4c0f6d82a95ce14200fa2488
Tshepo2022 <tshepotebe@gmail.com>
2023-05-22 15:10:58 +0200

    add province

--- Script.js
+++ Script.js
@@ -1,4 +1,20 @@
 function submit_by_tag()
 {
-alert('Button Clcked');
+
+    // const personal = [{}];
+
+    let name =  document.getElementById('studentName').value;
+    let surname = document.getElementById('studentName').value;
+    let school = document.getElementById('studentName').value;
+    let email = document.getElementById('studentName').value;
+    let password = document.getElementById('studentName').value;
+    let phone = document.getElementById('studentName').value;
+    let address = document.getElementById('studentName').value;
+    let city = document.getElementById('studentName').value;
+    
+
+    
+
+    
+
 }
