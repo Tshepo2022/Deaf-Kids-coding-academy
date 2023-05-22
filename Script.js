2f7cf1ba85640a841e8b681d0b6a5230f25044b8
Tshepo2022 <tshepotebe@gmail.com>
2023-05-22 16:05:50 +0200

    added submit

--- Script.js
+++ Script.js
@@ -1,20 +1,36 @@
 function submit_by_tag()
 {
+    const personal = [];
 
-    // const personal = [{}];
+    let obj = {
+        name: '',
+        school: '',
+        email: '',
+        password:'',
+        phone:'',
+        address: '',
+        city:'',
+        provinces:'',
+        zip:''
+
+    };
 
     let name =  document.getElementById('studentName').value;
-    let surname = document.getElementById('studentName').value;
-    let school = document.getElementById('studentName').value;
-    let email = document.getElementById('studentName').value;
-    let password = document.getElementById('studentName').value;
-    let phone = document.getElementById('studentName').value;
-    let address = document.getElementById('studentName').value;
-    let city = document.getElementById('studentName').value;
-    
+    let school = document.getElementById('school').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('Password').value;
+    let phone = document.getElementById('Phone').value;
+    let address = document.getElementById('Address').value;
+    let city = document.getElementById('City').value;
+    let provinces = document.getElementById('inputState').value;
+    let zip = document.getElementById('zip').value;
 
-    
+    personal.push(obj.name = name, obj.school = school,obj.email = email,obj.password = password,obj.phone = phone,obj.address = address,obj.city = city,obj.provinces = provinces,obj.zip = zip,)
+
+    alert(personal)
+
 
     
 
 }
+
